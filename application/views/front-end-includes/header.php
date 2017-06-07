37555b6b99ef99e664cb596714437b227d8cfa37
Aid Dorado <doradoaid@gmail.com>
2017-06-07 10:59:41 -0700

    Add header.php

---
+++ application/views/front-end-includes/header.php
@@ -0,0 +1,32 @@
+ <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <link rel="shortcut icon" href="images/favicon.ico">
+
+        <title>Go Local PH</title>
+
+        <!-- Google Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+
+        <!-- Bootstrap core CSS -->
+        <link href="<?php echo base_url(); ?>assets/css/front-end/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Owl Carousel CSS -->
+        <link href="<?php echo base_url(); ?>assets/css/front-end/owl.carousel.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>assets/css/front-end/owl.theme.default.min.css" rel="stylesheet">
+
+        <!-- Icon CSS -->
+        <link href="<?php echo base_url(); ?>assets/css/front-end/font-awesome.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="<?php echo base_url(); ?>assets/css/front-end/style.css" rel="stylesheet">
+
+
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+        <!--[if lt IE 9]>
+          <script src="js/html5shiv.js"></script>
+          <script src="js/respond.min.js"></script>
+        <![endif]-->
